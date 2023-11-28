3178926d3de6e94916411cba3ff9f707e4994a59
Usuario <Usuario@192.168.2.102>
2023-11-27 20:46:20 -0600

    DSD

--- src/main/java/com/mycompany/repositorio1/Repositorio1.java
+++ src/main/java/com/mycompany/repositorio1/Repositorio1.java
@@ -11,6 +11,6 @@ package com.mycompany.repositorio1;
 public class Repositorio1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello !");
     }
 }
